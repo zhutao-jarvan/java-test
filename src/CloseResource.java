bd5375d5467a0b82a6eb375c4710cb90b2dd1bfd
zhutao-jarvan <583043362@qq.com>
2018-02-03 16:17:21 +0800

    add file 	new file:   src/CloseResource.java

---
+++ src/CloseResource.java
@@ -0,0 +1,26 @@
+import javax.xml.ws.Service;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CloseResource {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ServerSocket server = new ServerSocket(8080);
+        InputStream socketInput = new Socket("localhost", 8080).getInputStream();
+
+        exec.execute(new IOBlocked(socketInput));
+        exec.execute(new IOBlocked(System.in));
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Shutdown all thread...");
+        exec.shutdownNow();
+        System.out.println("Closeing " + socketInput.getClass().getName());
+        socketInput.close();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Closing " + System.in.getClass().getName());
+        System.in.close();
+    }
+}
